398192301d34cd391b0b7b66c1bd6402bcca15c1
Ricky Catron <catron.ricky@gmail.com>
2016-05-25 14:46:01 -0400

    Added success file for easier managment

---
+++ after_success_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+if [ "$TRAVIS_BRANCH" == "dev" ]; then
+    export GIT_COMMITTER_EMAIL='travis@travis'
+    export GIT_COMMITTER_NAME='Travis CI'
+
+    # Since Travis does a partial checkout, we need to get the whole thing
+    repo_temp=$(mktemp -d)
+    git clone "https://github.com/sl33t/portfolio" "$repo_temp"
+
+    # shellcheck disable=SC2164
+    cd "$repo_temp"
+
+    printf 'Checking out master' >&2
+    git checkout master
+
+    printf 'Merging dev' >&2
+    git merge dev
+
+    printf 'Pushing to master' >&2
+
+    push_uri="https://$GITHUB_SECRET_TOKEN@github.com/sl33t/portfolio"
+
+    # Redirect to /dev/null to avoid secret leakage
+    git push "$push_uri" master >/dev/null 2>&1
+fi
