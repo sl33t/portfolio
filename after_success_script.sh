6d3d0ec5d0696b05ad563c2f081ec17f09313043
Ricky Catron <catron.ricky@gmail.com>
2016-05-27 19:56:25 -0400

    Messing with after success script to make sure dev is deployed to master

--- after_success_script.sh
+++ after_success_script.sh
@@ -10,6 +10,7 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
     # Since Travis does a partial checkout, we need to get the whole thing
     repo_temp=$(mktemp -d)
     git clone "https://github.com/sl33t/portfolio" "$repo_temp"
+    push_uri="https://$GITHUB_SECRET_TOKEN@github.com/sl33t/portfolio"
 
     # shellcheck disable=SC2164
     cd "$repo_temp"
@@ -21,19 +22,20 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
     printf 'Running pep8 and pyflakes fixes\n' >&2
     autopep8 --in-place --aggressive --recursive .
     autoflake --in-place --recursive --imports=django .
-    git commit -am "Pep8 and PyFlake corrections"
 
-    printf 'Checking out master\n' >&2
-    git checkout master
+    if [ -n "$(git status --porcelain)" ]; then
+        printf 'There are no changes\n' >&2
+    else
+        git commit -am "Pep8 and PyFlake corrections"  >&2
 
-    printf 'Merging dev\n' >&2
-    git merge dev
+        printf 'Checking out master\n' >&2
+        git checkout master
+
+        printf 'Merging dev\n' >&2
+        git merge dev
+        git push "$push_uri" dev >/dev/null 2>&1
+    fi
 
     printf 'Pushing to master\n' >&2
-
-    push_uri="https://$GITHUB_SECRET_TOKEN@github.com/sl33t/portfolio"
-
-    # Redirect to /dev/null to avoid secret leakage
     git push "$push_uri" master >/dev/null 2>&1
-    git push "$push_uri" dev >/dev/null 2>&1
 fi
