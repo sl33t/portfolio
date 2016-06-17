93fa71bfa4d5c6848f708d7c472ee02583f59ada
rcatron <catron.ricky@gmail.com>
2016-06-17 10:17:04 -0400

    Testing static urls

--- RickyCatron/urls.py
+++ RickyCatron/urls.py
@@ -1,23 +1,24 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from RickyCatron import settings
 from home import views
 
 urlpatterns = [
-                  url(r'^$', views.index, name="index"),
+    url(r'^$', views.index, name="index"),
 
-                  url(r'^portfolio/$', views.portfolio, name='portfolio'),
-                  url(r'^portfolio/(?P<item_id>\d+)/$',
-                      views.portfolio_item, name='portfolio_item'),
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
+    url(r'^portfolio/(?P<item_id>\d+)/$',
+        views.portfolio_item, name='portfolio_item'),
 
-                  url(r'^contact/$', views.contact, name='contact'),
-                  url(r'^send_mail/$', views.send_email, name='send_email'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^send_mail/$', views.send_email, name='send_email'),
 
-                  url(r'^admin/', admin.site.urls),
-              ] + static(settings.STATIC_URL,
-                         document_root=settings.STATIC_ROOT
-                         ) + static(settings.MEDIA_URL,
-                                    document_root=settings.MEDIA_ROOT
-                                    )
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
