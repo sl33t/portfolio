8a37fa3b222dc2626ac312eb63e5d7adc19c911d
rcatron <catron.ricky@gmail.com>
2016-06-17 10:07:30 -0400

    Added url field to portfolio items

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db.models import CharField, TextField, Model
+from django.db.models.fields import URLField
 
 from stdimage import StdImageField
 
@@ -6,6 +7,7 @@ from stdimage import StdImageField
 class PortfolioItem(Model):
     main_image_url = StdImageField(blank=True, variations={'large': (800, 800), 'medium': (500, 500)})
     title = CharField(max_length=100)
+    url = URLField()
     description = TextField()
     examples1 = StdImageField(blank=True, variations={'large': (500, 500)})
     examples2 = StdImageField(blank=True, variations={'large': (500, 500)})
