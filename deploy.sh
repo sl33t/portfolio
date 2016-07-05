e021db1c0d4baca4d12cdae35fef8949099822b8
rcatron <catron.ricky@gmail.com>
2016-07-05 12:31:53 -0400

    Trying a full checkout take 3

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 eval "$(ssh-agent -s)"
 chmod 600 $TRAVIS_BUILD_DIR/deploy_rsa
 mv $TRAVIS_BUILD_DIR/deploy_rsa ~/.ssh/id_rsa
-git checkout master
 git remote add deploy ssh://sl33t@web518.webfaction.com:/home/sl33t/webapps/portfolio/myproject
 git push deploy
