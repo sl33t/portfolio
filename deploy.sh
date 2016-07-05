1aefe83a208afa0b7003aa3ffe69e35e98748a52
rcatron <catron.ricky@gmail.com>
2016-07-05 12:22:09 -0400

    Trying a full checkout take 2

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 eval "$(ssh-agent -s)"
 chmod 600 $TRAVIS_BUILD_DIR/deploy_rsa
 mv $TRAVIS_BUILD_DIR/deploy_rsa ~/.ssh/id_rsa
-repo_temp=$(mktemp -d)
-git clone "https://github.com/sl33t/portfolio" "$repo_temp"
-cd "$repo_temp"
+git checkout master
 git remote add deploy ssh://sl33t@web518.webfaction.com:/home/sl33t/webapps/portfolio/myproject
 git push deploy
